22e0cac56b45f1dd903dcca2f669fae2028030ba
gugw <ggw_hsd@hotmail.com>
2018-12-10 17:25:57 +0800

    add ClientLib::SocketStart

--- VS2017Project/Practice/libEventProxy.h
+++ VS2017Project/Practice/libEventProxy.h
@@ -238,3 +238,72 @@ signal_cb1(evutil_socket_t sig, short events, void *user_data)
 	event_base_loopexit(base, &delay);
 }
 
+
+
+
+
+
+class ClientLib
+{
+public:
+	void init()
+	{
+#ifdef _WIN32
+		WSADATA wsa_data;
+		WSAStartup(0x0201, &wsa_data);
+#endif
+		puts("init a event_base!");
+		struct event_base *base; //定义一个event_base  
+		base = event_base_new(); //初始化一个event_base  
+		const char *x = event_base_get_method(base); //查看用了哪个IO多路复用模型，linux一下用epoll  
+		printf("METHOD:%s\n", x);
+		int y = event_base_dispatch(base); //事件循环。因为我们这边没有注册事件，所以会直接退出  
+		event_base_free(base);  //销毁libevent  
+		
+
+
+	}
+
+	int SocketStart()
+	{
+#ifdef _WIN32
+		WSADATA wsa_data;
+		WSAStartup(0x0201, &wsa_data);
+#endif
+
+		int client_fd; //定义一个客户端的SOCKET    
+		struct sockaddr_in server_addr; //服务器端    
+		memset(&server_addr, 0, sizeof(server_addr)); //数据初始化--清零      
+		server_addr.sin_family = AF_INET; //设置为IP通信      
+		inet_pton(server_addr.sin_family,"127.0.0.1", &server_addr.sin_addr);//服务器IP地址      
+		server_addr.sin_port = htons(9995); //服务器端口号      
+
+		client_fd = socket(PF_INET, SOCK_STREAM, 0);
+		if (client_fd < 1) {
+			puts("client socket error");
+			return 0;
+		}
+		/*将套接字绑定到服务器的网络地址上,并且连接服务器端*/
+		int ret = connect(client_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr));
+		if (ret < 0) {
+			puts("client connect error!");
+			return 0;
+		}
+
+		char buf[1024];
+		int len = recv(client_fd, buf, 1024, 0); //等待接收服务器端的数据    
+		buf[len] = '\0';
+		puts(buf);
+
+		string ss = "Hello World,saodsadoosadosa==sadsad==";
+		send(client_fd, ss.c_str(), strlen(ss.c_str()), 0); //发送数据    
+		memset(buf, 0, 1024);
+		int len2 = recv(client_fd, buf, 1024, 0); //继续接收服务端返回的数据    
+		buf[len2] = '\0';
+		puts(buf);
+
+		shutdown(client_fd, 2); //关闭socket    
+	
+
+	}
+};
