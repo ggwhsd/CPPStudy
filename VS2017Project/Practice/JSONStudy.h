e23e3b95e6d2e561f12932f6b858bc851b60c8ea
gugw <ggw_hsd@hotmail.com>
2018-12-11 13:47:37 +0800

    add STLVector.h and STLMap.h

--- VS2017Project/Practice/JSONStudy.h
+++ VS2017Project/Practice/JSONStudy.h
@@ -14,3 +14,82 @@ void testJsonRead()
 	}
 
 }
+
+
+/*
+{
+"pi": 3.141,
+"happy" : true,
+"name" : "Niels",
+"nothing" : null,
+"answer" : {
+"everything": 42
+},
+"list" : [1, 0, 2],
+"object" : {
+"currency": "USD",
+"value" : 42.99
+}
+}
+
+*/
+void JsonCreateMethod1()
+{
+	json j;
+	j["pi"] = 3.141;
+	j["happy"] = true;
+	j["name"] = "Niels";
+	j["nothing"] = nullptr;
+	j["answer"]["everything"] = 42;
+	j["list"] = { 1,2,3 };
+	j["object"] = { {"currency","USD"},{"value",42.99} };
+
+
+}
+void JsonCreateMethod2()
+{
+
+	json j2 = {
+		{ "pi", 3.141 },
+		{ "happy" , true },
+		{ "name" , "Niels" },
+		{ "nothing",nullptr },
+		{ "answer",{
+					{ "everything",42 }
+					},
+		{ "list" ,{ 1, 0, 2 } },
+		{ "object" ,{
+					{ "currency", "USD" },
+					{ "value",42.99 }
+					}
+		}
+		}
+	};
+}
+
+void JsonCreateMethod3()
+{
+	// a way to express the empty array []
+	json empty_array_explicit = json::array();
+
+	// ways to express the empty object {}
+	json empty_object_implicit = json({});
+	json empty_object_explicit = json::object();
+
+	// a way to express an _array_ of key/value pairs [["currency", "USD"], ["value", 42.99]]
+	json array_not_object = json::array({ { "currency", "USD" },{ "value", 42.99 } });
+}
+
+void SerializationMethod1()
+{
+	// create object from string literal
+	json j = "{ \"happy\": true, \"pi\": 3.141 }"_json;
+
+	// or even nicer with a raw string literal
+	auto j2 = R"(
+  {
+    "happy": true,
+    "pi": 3.141
+  }
+)"_json;
+}
