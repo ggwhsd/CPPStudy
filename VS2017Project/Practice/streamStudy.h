4a5d4f7c6e07cbf8485f6b62f4d73b3b2262188b
gugw <ggw_hsd@hotmail.com>
2018-12-17 17:05:04 +0800

    update

--- VS2017Project/Practice/streamStudy.h
+++ VS2017Project/Practice/streamStudy.h
@@ -8,8 +8,22 @@ using namespace std;
 void ostreamTest()
 {
 	ostringstream ostr;
+
 	ostr << "asdf" << __FUNCTION__ << "ddd" << 1;
 	string gstr = ostr.str();
-	cout << gstr;
+	cout << "1" << ostr.str() << endl;;
+	ostr.clear();
+	cout << "2"<< ostr.str() << endl;;
+	ostr.str("");
+	cout << "3" << ostr.str() << endl;;
+	//下面是比较特殊的
+	ostr.str("ggggg");
+	ostr << "ffffff" << endl;
+	cout << "4" << ostr.str() << endl;;
+	
+
+
 	
 }
+
+
