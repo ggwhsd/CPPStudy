65dc09f81f1a517e940e8aba83aef5c51b0227d1
gugw <ggw_hsd@hotmail.com>
2019-08-21 14:13:53 +0800

    performace of vector and new

--- VS2017Project/Practice/STLVector.h
+++ VS2017Project/Practice/STLVector.h
@@ -85,11 +85,18 @@ void testVector2()
 //没有使用reserve的方式之后，初始化时间
 void testVectorTime1(int out)
 {
+
+	milliClock_type start, end;
+	std::chrono::milliseconds timeElpased_micro(1);
+
+	cout << "***************使用vector，并且未reverse空间**************************" << endl;
+
 	vector<MyStruct> tenElement;
 	//tenElement.reserve(10);
 	cout << "tenElement.size():" << tenElement.size() << endl;
 	cout << "tenElement.capacity():" << tenElement.capacity() << endl;
-	clock_t start = clock();
+	start = chrono::time_point_cast<chrono::milliseconds>(chrono::steady_clock::now());
+
 	MyStruct s = { 1,1,"a" };
 	s.d = "a0";
 	tenElement.push_back(s);
@@ -100,58 +107,95 @@ void testVectorTime1(int out)
 	int i = 1;
 	for (; i < out; i++)
 	{
-		s.d = "a" + to_string(i);
+		//s.d = "a" + to_string(i);
 		tenElement.push_back(s);
 	}
-	clock_t end = clock();
-	cout << "生成tenElement" << out << "个对象 耗时" << end - start << "MILLISECOND" << endl;
-	start = clock();
+	end = chrono::time_point_cast<chrono::milliseconds>(chrono::steady_clock::now());
+	timeElpased_micro = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	cout << "生成tenElement" << out << "个对象 耗时" << timeElpased_micro.count() << "MILLISECOND" << endl;
+	start = chrono::time_point_cast<chrono::milliseconds>(chrono::steady_clock::now());
 	for (size_t i = 0; i < out; i++)
 	{
 		if (tenElement[i].a == out)
 			break;
 	}
-	end = clock();
-	cout << "生成tenElement" << out << "个对象 耗时" << end - start << "MILLISECOND" << endl;
+	end = chrono::time_point_cast<chrono::milliseconds>(chrono::steady_clock::now());
+	timeElpased_micro = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	cout << "遍历tenElement" << out << "个对象 耗时" << timeElpased_micro.count() << "MILLISECOND" << endl;
 
 	it = tenElement.begin();
 	cout << it._Ptr << endl;
 	tenElement.erase(it);
+	cout <<endl;
 	//vector<MyStruct>::iterator it = tenElement.begin();
 
 }
 
 //使用reserve的方式之后，初始化时间
-void testVectorTime2()
+void testVectorTime2(int out)
 {
+	milliClock_type start, end;
+	std::chrono::milliseconds timeElpased_micro(1);
+	cout << "***************使用vector，并且预先reverse空间**************************" << endl;
 	vector<MyStruct> tenElement;
-	tenElement.reserve(100000);
+	tenElement.reserve(out);
 	cout << "tenElement.size():" << tenElement.size() << endl;
 	cout << "tenElement.capacity():" << tenElement.capacity() << endl;
-	clock_t start = clock();
+	start = chrono::time_point_cast<chrono::milliseconds>(chrono::steady_clock::now());
 	MyStruct s = { 1,1,"a" };
 	int i = 0;
-	for (; i < 100000; i++)
+	for (; i < out; i++)
 	{
+		end = chrono::time_point_cast<chrono::milliseconds>(chrono::steady_clock::now());
 		s.a = i;
 		tenElement.push_back(s);
 	}
-	clock_t end = clock();
-	cout << "生成tenElement 1000000个对象 耗时" << end - start << "MILLISECOND" << endl;
-	start = clock();
-	for (size_t i = 99999; i > 0; i--)
+	end = chrono::time_point_cast<chrono::milliseconds>(chrono::steady_clock::now());
+	timeElpased_micro = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	cout << "生成tenElement "<< out <<"个对象 耗时" << timeElpased_micro.count() << "MILLISECOND" << endl;
+	start = chrono::time_point_cast<chrono::milliseconds>(chrono::steady_clock::now());
+	for (size_t i = out-1; i > 0; i--)
 	{
 		if (tenElement[i].a == 2)
 		{
-			cout << tenElement[i].a << endl;
+			//cout << tenElement[i].a << endl;
 			break;
 		}
 	}
-	end = clock();
-
-	cout << "遍历tenElement 1000000个对象 耗时" << end - start << "MILLISECOND" << endl;;
-
+	end = chrono::time_point_cast<chrono::milliseconds>(chrono::steady_clock::now());
+	timeElpased_micro = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	cout << "遍历tenElement "<< out <<"个对象 耗时" << timeElpased_micro.count()<< "MILLISECOND" << endl;;
+	cout << endl;
 	//vector<MyStruct>::iterator it = tenElement.begin();
+
+}
+
+//没有使用reserve的方式之后，初始化时间
+void testArrayTime1(int out)
+{
+	milliClock_type start, end;
+	std::chrono::milliseconds timeElpased_micro(1);
+	cout << "***********使用new 数组指针方式*******************" << endl;
+	start = chrono::time_point_cast<chrono::milliseconds>(chrono::steady_clock::now());
+	MyStruct* tenElement = new MyStruct[out]();
+	int i = 1;
+	for (; i < out; i++)
+	{
+		tenElement[i].a = 1;
+	}
+	end = chrono::time_point_cast<chrono::milliseconds>(chrono::steady_clock::now());
+	timeElpased_micro = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	cout << "生成tenElement" << out << "个对象 耗时" << timeElpased_micro.count() << "MILLISECOND" << endl;
+	start = chrono::time_point_cast<chrono::milliseconds>(chrono::steady_clock::now());
+	for (size_t i = 0; i < out; i++)
+	{
+		if (tenElement[i].a == out)
+			break;
+	}
+	end = chrono::time_point_cast<chrono::milliseconds>(chrono::steady_clock::now());
+	timeElpased_micro = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	cout << "遍历tenElement" << out << "个对象 耗时" << timeElpased_micro.count() << "MILLISECOND" << endl;
+	cout << endl;
 
 }
 
@@ -195,7 +239,7 @@ void testVectorErase(int out)
 	cout << "after tenElement.erase(tenElement.begin());" << endl;
 	it = tenElement.begin();
 	cout << it->d << it._Ptr << endl;
-	
+	cout << endl;
 
 }
 
